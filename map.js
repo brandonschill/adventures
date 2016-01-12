cec85839387744f3201a3fe9ca745c7c1ddcf78c
brandon schill <brandoschill@gmail.com>
2016-01-12 14:00:52 -0800

    #1 Added search functionality to external search bar

--- map.js
+++ map.js
@@ -1,41 +1,166 @@
-var map;
-var infowindow;
+// This sample uses the Place Autocomplete widget to allow the user to search
+// for and select a place. The sample then displays an info window containing
+// the place ID and other information about the place that the user has
+// selected.
 
 function initMap() {
-  var yosemite = {lat: 37.8499, lng: -119.5677};
+	var map = new google.maps.Map(document.getElementById('map'), {
+		center: {lat: 37.8499, lng: -119.5677},
+		zoom: 12
+	});
 
-  map = new google.maps.Map(document.getElementById('map'), {
-    center: yosemite,
-    zoom: 10
+	var input = document.getElementById('pac-input');
+
+	var autocomplete = new google.maps.places.Autocomplete(input);
+	autocomplete.bindTo('bounds', map);
+
+
+	var infowindow = new google.maps.InfoWindow();
+
+	// var service = new google.maps.places.PlacesService(map);
+	// service.nearbySearch({
+	// 	location: input,
+	// 	radius: 500,
+	// 	types: ['hiking']
+	// }, callback);
+
+	var marker = new google.maps.Marker({
+		map: map
+	});
+	marker.addListener('submit', function() {
+		infowindow.open(map, marker);
+	});
+
+	autocomplete.addListener('place_changed', function() {
+		infowindow.close();
+		var place = autocomplete.getPlace();
+		if (!place.geometry) {
+			return;
+		}
+
+		if (place.geometry.viewport) {
+			map.fitBounds(place.geometry.viewport);
+		} else {
+			map.setCenter(place.geometry.location);
+			map.setZoom(10);
+		}
+
+    // Set the position of the marker using the place ID and location.
+    marker.setPlace({
+    	placeId: place.place_id,
+    	location: place.geometry.location
+    });
+    marker.setVisible(true);
+
+    infowindow.setContent('<div><strong>' + place.name + '</strong><br>' +
+    	'Place ID: ' + place.place_id + '<br>' +
+    	place.formatted_address);
+    infowindow.open(map, marker);
   });
-
-  infowindow = new google.maps.InfoWindow();
-
-  var service = new google.maps.places.PlacesService(map);
-  service.nearbySearch({
-    location: yosemite,
-    radius: 750000,
-    types: ['hiking']
-  }, callback);
 }
 
-function callback(results, status) {
-  if (status === google.maps.places.PlacesServiceStatus.OK) {
-    for (var i = 0; i < results.length; i++) {
-      createMarker(results[i]);
-    }
-  }
-}
 
-function createMarker(place) {
-  var placeLoc = place.geometry.location;
-  var marker = new google.maps.Marker({
-    map: map,
-    position: place.geometry.location
-  });
+// var map;
+// var infowindow;
 
-  google.maps.event.addListener(marker, 'click', function() {
-    infowindow.setContent(place.name);
-    infowindow.open(map, this);
-  });
-}
+// function initMap() {
+//   var yosemite = {lat: 37.8499, lng: -119.5677};
+
+//   map = new google.maps.Map(document.getElementById('map'), {
+//     center: yosemite,
+//     zoom: 10
+//   });
+
+//   infowindow = new google.maps.InfoWindow();
+
+//   var service = new google.maps.places.PlacesService(map);
+//   service.nearbySearch({
+//     location: yosemite,
+//     radius: 750000,
+//     types: ['hiking']
+//   }, callback);
+// }
+
+// function callback(results, status) {
+//   if (status === google.maps.places.PlacesServiceStatus.OK) {
+//     for (var i = 0; i < results.length; i++) {
+//       createMarker(results[i]);
+//     }
+//   }
+// }
+
+// function createMarker(place) {
+//   var placeLoc = place.geometry.location;
+//   var marker = new google.maps.Marker({
+//     map: map,
+//     position: place.geometry.location
+//   });
+
+//   google.maps.event.addListener(marker, 'click', function() {
+//     infowindow.setContent(place.name);
+//     infowindow.open(map, this);
+//   });
+// }
+
+// function initAutocomplete() {
+//   var map = new google.maps.Map(document.getElementById('map'), {
+//     center: {lat: 37.8499, lng: -119.5677},
+//     zoom: 10,
+//     mapTypeId: google.maps.MapTypeId.ROADMAP
+//   });
+
+//   // Create the search box and link it to the UI element.
+//   var input = document.getElementById('pac-input');
+//   var searchBox = new google.maps.places.SearchBox(input);
+//   map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
+
+//   // Bias the SearchBox results towards current map's viewport.
+//   map.addListener('bounds_changed', function() {
+//     searchBox.setBounds(map.getBounds());
+//   });
+
+//   var markers = [];
+//   // Listen for the event fired when the user selects a prediction and retrieve
+//   // more details for that place.
+//   searchBox.addListener('places_changed', function() {
+//     var places = searchBox.getPlaces();
+
+//     if (places.length == 0) {
+//       return;
+//     }
+
+//     // Clear out the old markers.
+//     markers.forEach(function(marker) {
+//       marker.setMap(null);
+//     });
+//     markers = [];
+
+//     // For each place, get the icon, name and location.
+//     var bounds = new google.maps.LatLngBounds();
+//     places.forEach(function(place) {
+//       var icon = {
+//         url: place.icon,
+//         size: new google.maps.Size(71, 71),
+//         origin: new google.maps.Point(0, 0),
+//         anchor: new google.maps.Point(17, 34),
+//         scaledSize: new google.maps.Size(25, 25)
+//       };
+
+//       // Create a marker for each place.
+//       markers.push(new google.maps.Marker({
+//         map: map,
+//         icon: icon,
+//         title: place.name,
+//         position: place.geometry.location
+//       }));
+
+//       if (place.geometry.viewport) {
+//         // Only geocodes have viewport.
+//         bounds.union(place.geometry.viewport);
+//       } else {
+//         bounds.extend(place.geometry.location);
+//       }
+//     });
+//     map.fitBounds(bounds);
+//   });
+// }
