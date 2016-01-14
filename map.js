6b394550f40d29c57307a4cee336cd954aa82bd4
brandon schill <brandoschill@gmail.com>
2016-01-14 10:57:47 -0800

    #1 Added map

--- map.js
+++ map.js
@@ -9,49 +9,11 @@ function initMap() {
 		zoom: 12
 	});
 
-var input = document.getElementById('pac-input');
+	var input = document.getElementById('pac-input');
 
+	var completed = new google.maps.places.Autocomplete(input, null);
 
-var marker = new google.maps.Marker({
-	map: map
-});
-
-var searchBox = new google.maps.places.SearchBox(input, {});
-
-searchBox.addListener('places_changed', function() {
-	var places = searchBox.getPlaces();
-
-	if (places.length == 0) {
-		return;
-	}
-
-	markers.forEach(function(marker) {
-		marker.setMap(null);
+	completed.addListener('place_changed', function() {
+		console.log(completed.getPlace());
 	});
-	markers = [];
-
-	var bounds = new google.maps.LatLngBounds();
-	places.forEach(function(place) {
-		var icon = {
-			url: place.icon,
-			size: new google.maps.Size(71, 71),
-			origin: new google.maps.Point(0, 0),
-			anchor: new google.maps.Point(17, 34),
-			scaledSize: new google.maps.Size(25, 25)
-		};
-
-		markers.push(new google.maps.Marker({
-			map: map,
-			icon: icon,
-			title: place.name,
-			position: place.geometry.location
-		}));
-
-		if (place.geometry.viewport) {
-			bounds.union(place.geometry.viewport);
-		} else {
-			bounds.extend(place.geometry.location);
-		}
-	});
-	map.fitBounds(bounds);
-});
+}
