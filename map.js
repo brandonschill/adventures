e67952469e1c0878a1e110d4df8b433fc6497a45
brandon schill <brandoschill@gmail.com>
2016-01-14 10:29:40 -0800

    Map functionality

--- map.js
+++ map.js
@@ -9,87 +9,49 @@ function initMap() {
 		zoom: 12
 	});
 
-	var input = document.getElementById('pac-input');
+var input = document.getElementById('pac-input');
 
-	var autocomplete = new google.maps.places.Autocomplete(input);
-	autocomplete.bindTo('bounds', map);
 
-	var infowindow = new google.maps.InfoWindow();
+var marker = new google.maps.Marker({
+	map: map
+});
 
-	var marker = new google.maps.Marker({
-		map: map
+var searchBox = new google.maps.places.SearchBox(input, {});
+
+searchBox.addListener('places_changed', function() {
+	var places = searchBox.getPlaces();
+
+	if (places.length == 0) {
+		return;
+	}
+
+	markers.forEach(function(marker) {
+		marker.setMap(null);
 	});
-	marker.addListener('click', function() {
-		infowindow.open(map, marker);
-	});
+	markers = [];
 
-	autocomplete.addListener('place_changed', function() {
-		infowindow.close();
-		var place = autocomplete.getPlace();
-		if (!place.geometry) {
-			return;
-		}
+	var bounds = new google.maps.LatLngBounds();
+	places.forEach(function(place) {
+		var icon = {
+			url: place.icon,
+			size: new google.maps.Size(71, 71),
+			origin: new google.maps.Point(0, 0),
+			anchor: new google.maps.Point(17, 34),
+			scaledSize: new google.maps.Size(25, 25)
+		};
+
+		markers.push(new google.maps.Marker({
+			map: map,
+			icon: icon,
+			title: place.name,
+			position: place.geometry.location
+		}));
 
 		if (place.geometry.viewport) {
-			map.fitBounds(place.geometry.viewport);
+			bounds.union(place.geometry.viewport);
 		} else {
-			map.setCenter(place.geometry.location);
-			map.setZoom(10);
+			bounds.extend(place.geometry.location);
 		}
-
-    // Set the position of the marker using the place ID and location.
-    marker.setPlace({
-    	placeId: place.place_id,
-    	location: place.geometry.location
-    });
-    marker.setVisible(true);
-
-    infowindow.setContent('<div><strong>' + place.name + '</strong><br>' +
-    	'Place ID: ' + place.place_id + '<br>' +
-    	place.formatted_address);
-    infowindow.open(map, marker);
-  });
-}
-
-
-// var map;
-// var infowindow;
-
-// function initMap() {
-//   var yosemite = {lat: 37.8499, lng: -119.5677};
-
-//   map = new google.maps.Map(document.getElementById('map'), {
-//     center: yosemite,
-//     zoom: 10
-//   });
-
-//   infowindow = new google.maps.InfoWindow();
-
-//   var service = new google.maps.places.PlacesService(map);
-//   service.nearbySearch({
-//     location: yosemite,
-//     radius: 750000,
-//     types: ['hiking']
-//   }, callback);
-// }
-
-// function callback(results, status) {
-//   if (status === google.maps.places.PlacesServiceStatus.OK) {
-//     for (var i = 0; i < results.length; i++) {
-//       createMarker(results[i]);
-//     }
-//   }
-// }
-
-// function createMarker(place) {
-//   var placeLoc = place.geometry.location;
-//   var marker = new google.maps.Marker({
-//     map: map,
-//     position: place.geometry.location
-//   });
-
-//   google.maps.event.addListener(marker, 'click', function() {
-//     infowindow.setContent(place.name);
-//     infowindow.open(map, this);
-//   });
-// }
+	});
+	map.fitBounds(bounds);
+});
