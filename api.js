1c1287141ec54eb0012fe15123a9326a992c4594
Brandon Schill <brandoschill@gmail.com>
2016-01-27 09:51:42 -0800

    Update api.js

--- api.js
+++ api.js
@@ -3,7 +3,7 @@ var router = express.Router();
 var request = require('request');
 
 router.get('/', function(req, res) {	
-	request('https://maps.googleapis.com/maps/api/js?key=AIzaSyAv-75HJMgoaYSx8djLXA02fTZAJIQCJSU&callback=initMap', function(error, response, body) {
+	request('https://maps.googleapis.com/maps/api/js?key=xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx&callback=initMap', function(error, response, body) {
 		if (!error && response.statusCode == 200) {
 			console.log(body);
 			res.send(body);
